fdb0ea886c4efe378a23313740b650376dcf23c5
Jacinda Zhong <jacindaz@gmail.com>
2014-11-22 15:28:30 -0500

    Blank template of quiz problem 5-1

---
+++ quiz/quiz-problem5-1.py
@@ -0,0 +1,18 @@
+"""
+You are taking a class that plans to assign final grades based on two midterm
+quizzes and a final exam. The final grade will be based on 25% for each midterm,
+and 50% for the final. You are told that the grades on the exams were each
+uniformly distributed integers:
+
+    Midterm 1: 50 <= grade <= 80
+    Midterm 2: 60 <= grade <= 90
+    Final Exam: 55 <= grade <= 95
+
+Write a function called sampleQuizzes() that implements a Monte Carlo simulation
+that estimates the probability of a student having a final score >= 70 and <= 75.
+Assume that 10,000 trials are sufficient to provide an accurate answer.
+"""
+
+def sampleQuizzes():
+    # Your code here
+    pass
